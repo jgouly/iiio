f8c3d70ab856afd051c3337d8ab28f603f02d03b
captainjey <cardologist@gmail.com>
2009-01-20 13:43:45 +0000

    Added handle_{input output} and added @channels

--- iiio.rb
+++ iiio.rb
@@ -1,30 +1,17 @@
 require 'socket';require 'util'
 @socket = TCPSocket.new ARGV[0], 6667
 @server_f = open ARGV[0], "a"
-["NICK #{ARGV[1]}", "USER #{ARGV[1]} 0 * :iiio user", "JOIN #{ARGV[2]}"].each { |msg| log msg, @socket }
+@channels = {}
+["NICK #{ARGV[1]}", "USER #{ARGV[1]} 0 * :iiio user"].each { |msg| log msg, @socket }
 
 while input = select([@socket, STDIN], nil, nil)
-  input[0].each do |i|
-    if i == @socket
-      log line = @socket.gets, @server_f
-      case line
-        when /^PING :(.+)$/i: @socket.puts "PONG :#{$1}"
-        when /:([^!]*)![^ ].* +PRIVMSG ([^ :]+) +:(.*)/:
-	  m, nick, target, msg = *line.match(/:([^!]*)![^ ].* +PRIVMSG ([^ :]+) +:(.*)/)
-	  if m
-	    if action = msg[/^\001ACTION(.+)\001/, 1]
-	      log_m "#{target} * #{nick}#{action}", @server_f
-	    else
-	      log_m "#{target} <#{nick}> #{msg}", @server_f
-  	    end 
-	  else
-	    log line, @server_f
-	  end
-      end
-    else
-      log STDIN.gets, @socket
-    end
-  end
-@server_f.flush
+	input[0].each do |i|
+		if i == @socket
+			handle_output @socket.gets.strip
+		else
+			handle_input STDIN.gets.strip
+		end
+	end
+	@server_f.flush
 end
 @server_f.close
--- util.rb
+++ util.rb
@@ -1,7 +1,49 @@
 def log(input,  out)
-  out.puts input
+	out.puts input
 end
 
-def log_m(msg, out)
- log "#{Time.now.strftime("%d/%m/%y %H:%M:%S")} #{msg}", out
+def log_m(target, msg, out)
+	log "#{target} #{Time.now.strftime("%d/%m/%y %H:%M:%S")} #{msg}", out
+end
+
+def handle_input(input)
+	case input
+		when /^\/me (.+) (.*)/i: log "PRIVMSG #{channel_name $1} :\001ACTION #{$2}\001", @socket
+		when /^\/(j|p) (.+)/i:   log "#{join_part $1, channel_name($2)} #{channel_name $2}", @socket
+		when /^\/chans/: log @channels.values.join(", "), @server_f
+	else
+		log input, @socket
+	end
+end
+
+def handle_output(output)
+	case output
+		when /^PING :(.+)$/i: @socket.puts "PONG :#{$1}"
+		when /:([^!]*)![^ ].* +PRIVMSG ([^ :]+) +:(.*)/:
+			nick, target, msg = $1, $2, $3
+			if action = msg[/^\001ACTION(.+)\001/, 1]
+				log output, @server_f
+				log_m target, "* #{nick}#{action}", @server_f
+			else
+				log_m target, "<#{nick}> #{msg}", @server_f
+			end
+		else log output, @server_f
+	end
+end
+
+def channel_name(name)
+  case name 
+		when /^#/: name
+		else @channels[name] || "##{name}"
+	end
+end
+
+def join_part(join_part, channel)
+	if join_part == 'j'
+		@channels[channel] = channel
+		'JOIN'	
+	else
+		@channels.delete channel
+		'PART'
+	end
 end
